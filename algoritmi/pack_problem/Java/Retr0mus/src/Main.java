c4dfa3cba2207862fed6f2f98564f3cb82375b17
Retr0mus <valeriorex03@gmail.com>
2022-10-27 17:17:03 +0200

    Implementata soluzione pack problem

---
+++ algoritmi/pack_problem/Java/Retr0mus/src/Main.java
@@ -0,0 +1,51 @@
+// Verifica: a < b; a, b > 0; pack > 0.
+// Tutti int.
+// Se pack % a == 0 allora la soluzione è pack/a.
+// Se pack % a != 0 allora se a e' pari e b e' dispari o viceversa (n-1)*a + b se < pack,
+// altrimenti resta la soluzione di prima.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Dichiarazione delle variabili
+        Scanner scanner = new Scanner(System.in); // Inizializzazione scanner
+        int pack, a, b;
+        int na = 0, nb = 0;
+
+        // Lettura dati da stdin
+        do {
+            System.out.println("Inserisci la massa in kg del pack: ");
+            pack = scanner.nextInt();
+        }while(pack <= 0);
+
+        do {
+            System.out.println("Inserisci la massa in kg dei blocchi A: ");
+            a = scanner.nextInt();
+        }while(a <= 0);
+
+        do {
+            System.out.println("Inserisci la massa in kg dei blocchi B (> A): ");
+            b = scanner.nextInt();
+        }while(b <= 0 || b <= a);
+
+        // Elaborazione del risultato
+        if(pack % a == 0)
+            na = pack/a;
+        else
+        {
+            int i = 1;
+
+            while(((pack/a)-i)*a + b*i < pack)
+            {
+                i++;
+            }
+
+            na = (pack/a)-i;
+            nb = i;
+        }
+
+        // Stampa del risultato
+        System.out.println("{" + na + "|" + nb + "}");
+    }
+}
