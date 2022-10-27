61e3667bc9218884fcf5f9351df7585682202be9
Lorenzo <lupi.lorenzo001@gmail.com>
2022-10-27 16:29:09 +0200

    Fixed everything (trusta)

--- tutorial/LetterCount/4C4C/LetterCount.c
+++ tutorial/LetterCount/4C4C/LetterCount.c
@@ -17,8 +17,8 @@ int main(int argc, char** args){
 	int Var, vAr, vaR;
 	vAr = vaR = Var = 0;
 	se(argc != 2) exit(0);
-	printf("Num. caratteri= %d",(int)strlen(args[1]));
-	per(int i = 0; i < strlen(args[1]); i++) se(isMhh(args[1][i])){ se(isWhaat(args[1][i])) vAr++; oppure vaR++;} oppure se(args[1][i] == ' ') Var++;
+	printf("Num. caratteri= %d\n",(int)strlen(args[1]));
+	per(int i = 0; i < strlen(args[1]); i++) se(isMhh(args[1][i])){ se(isWhaat(args[1][i])) {vAr++;} oppure vaR++;} oppure se(args[1][i] == ' ') Var++;
 	printf("Numero vocali: %d\nNumero Consonanti: %d\nNumero blanks: %d\n", vAr, vaR, Var);
 }
 
@@ -26,5 +26,5 @@ int isMhh(char c){
 	se( (c>=65 ee c<=90) oo (c>=97 ee c<= 97+25) ) restituisci 1; restituisci 0;
 }
 int isWhaat(char c){
-	se(c == 65 oo c == 69 oo c == 73 oo c == 79 oo c == 85) restituisci 1; restituisci 0;
+	se( ((c e 0xF) == (65 & 0xF))  oo ((c& 0xF) == (69& 0xF)) oo ((c& 0xF)) == ((73& 0xF)) oo ((c& 0xF) == (79& 0xF)) oo ((c& 0xF) == (85))) restituisci 1; restituisci 0;
 }
