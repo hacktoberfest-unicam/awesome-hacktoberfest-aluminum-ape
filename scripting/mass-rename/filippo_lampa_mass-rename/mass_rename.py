9fbd45b552a5f9925c77c47eb5b1145e78376d3c
FilippoLampa <pippolampa@gmail.com>
2022-10-27 16:54:12 +0200

    Filippo Lampa Mass Rename Fix

--- scripting/mass-rename/filippo_lampa_mass-rename/mass_rename.py
+++ scripting/mass-rename/filippo_lampa_mass-rename/mass_rename.py
@@ -6,5 +6,5 @@ path = input("Insert the path")
 
 files = os.listdir(path)
 
-for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, file).replace(" ", separator)) 
+for file in files:
+    os.rename(os.path.join(path, file), os.path.join(path, file.replace(' ', separator))) 
