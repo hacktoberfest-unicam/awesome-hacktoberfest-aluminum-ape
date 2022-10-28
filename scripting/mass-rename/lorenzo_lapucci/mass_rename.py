0ff42f3a8427d099546a633c40eaa9a07941189b
Lorenzo Lapucci <halexercraft@gmail.com>
2022-10-28 17:25:09 +0100

    Update mass_rename.py

--- scripting/mass-rename/lorenzo_lapucci/mass_rename.py
+++ scripting/mass-rename/lorenzo_lapucci/mass_rename.py
@@ -2,6 +2,6 @@ import os
 
 replacement = input("Insert the replacement string: ")
 
-path = input("Choose the path")
+path = input("Choose the path: ")
 
 [os.rename(f, f.replace(' ', replacement)) for f in os.listdir(path)]
